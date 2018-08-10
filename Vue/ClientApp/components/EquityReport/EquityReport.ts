3dc3aab42c23c29884ad6e3680b26bb8052fb470
Kiran S J <kiransj@outlook.com>
2018-08-10 11:00:42 +0530

    Updated UI

--- Vue/ClientApp/components/EquityReport/EquityReport.ts
+++ Vue/ClientApp/components/EquityReport/EquityReport.ts
@@ -22,16 +22,36 @@ let stockReport:StockReport[] = [];
 @Component
 export default class ReportComponent extends Vue {
     stockReport:StockReport[] = [];
+    startIndex: number = 0;
+    elementsPerPage: number = 50;
     created(): void {
-        //this.symbol = this.$route.params.symbol;
-        fetch('/api/StockData/GetLastestStockReport')
-        .then(response => response.json() as Promise<StockReport[]>)
-        .then(data => {
-            stockReport = this.stockReport = data;
-        });
+        if(stockReport.length == 0)
+        {
+            //this.symbol = this.$route.params.symbol;
+            fetch('/api/StockData/GetLastestStockReport')
+            .then(response => response.json() as Promise<StockReport[]>)
+            .then(data => {
+                stockReport = data;
+                this.stockReport = stockReport;
+            });
+        }
+        this.stockReport = stockReport;
     }
 
     mounted() {
 
+    }
+
+    ShowElements(more: boolean): void {
+        if(more)
+        {
+            if((this.startIndex + 50) < this.stockReport.length)
+                this.startIndex += 50;
+        } else
+        {
+            this.startIndex -= 50;
+            if(this.startIndex < 0)
+                this.startIndex = 0;
+        }
     }
 }
